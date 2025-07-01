4f469d51c4ed48f3e8ebd768515742634f81ef20
HydrogenMacro <hydrogenmacro@gmail.com>
2025-07-01 19:50:07 -0400

    git stash is getting sketch so imma commit everything before i lose it all

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import recursiveFontUrl from "./assets/Recursive.ttf";
+import { Canvas } from "@react-three/fiber";
+import { OrthographicCamera, Text } from "@react-three/drei";
+
+const recursiveFont = new FontFace("Recursive", `url(${recursiveFontUrl})`);
+document.fonts.add(recursiveFont);
+recursiveFont.load();
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <div
+                className="title-card"
+                style={{
+                    width: "100%",
+                    height: "400px",
+                }}
+            >
+                <Canvas
+                    style={{
+                        position: "absolute",
+                    }}
+                >
+                    <OrthographicCamera makeDefault position={[0,0,10]} lookAt={[0, 0,0]}>
+
+                    </OrthographicCamera>
+                    <mesh>
+                        <Text
+                            fontSize={60}
+                            font={recursiveFontUrl}
+                            fontWeight={600}
+                            color="black"
+                            anchorX="center"
+                            anchorY="middle"
+                            characters="DBGUI"
+                        >
+                            dbgui
+                        </Text>
+                    </mesh>
+                </Canvas>
+                <div
+                    style={{
+                        width: "100%",
+                        height: "100%",
+                        position: "absolute",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        fontSize: 60,
+                        fontWeight: "600"
+                    }}
+                >
+                    <h1
+                        style={{
+                            fontFamily: `Recursive, monospace`,
+                        }}
+                    >
+                        dbgui
+                    </h1>
+                </div>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
